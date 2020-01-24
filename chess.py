c56bdd65ba16bab4ffe5be3e75d4c873d37ef017
captain-bari <54902053+captain-bari@users.noreply.github.com>
2020-01-24 22:29:50 +0530

    Update chess.py

--- chess.py
+++ chess.py
@@ -3,8 +3,10 @@ from pygame.locals import *
 
 pygame.init()
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode((640, 640))
-wh=pygame.image.load("wh.png")
+screen = pygame.display.set_mode((480, 480))
+screen.fill((50,50,50))
+wh=pygame.image.load("bb.png")
+
 done = False
 
 while not done:
@@ -12,21 +14,21 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
 
-    x=81
+    x=60
     y=0
     line=1
     for temp in range(64):
-        pygame.draw.rect(screen, (255, 255, 255),(x,y,80,80))
-        x+=160
-        if x>560:
+        pygame.draw.rect(screen, (255, 255, 255),(x,y,60,60))
+        x+=120
+        if x>420:
             line+=1
-            y+=80
+            y+=60
             if line%2==0:
                 x=0
             else:
-                x=80
+                x=60
 
-    screen.blit(wh,((0,0)))
+    screen.blit(wh,((60,0)))
 
     msElapsed = clock.tick(30)
     pygame.display.update()
