aded0957ed1a4476388f9ccf00df4396f20954e4
Gurmeet-Singh745 <60310651+Gurmeet-Singh745@users.noreply.github.com>
2020-01-26 13:55:44 +0530

    Update rules.py

--- rules.py
+++ rules.py
@@ -6,15 +6,15 @@ def check(pieces, firsttap, from_x, from_y, to_x, to_y):
   first_char = final_pos[0];
   if(firsttap == "wp" and first_char != "w"):
     if(from_x == 6):
-      if(to_x==4 and to_y==from_y):
+      if(to_x==4 and to_y == from_y):
         return True
-    if (to_x == (from_x - 1) and (abs(to_y - from_y)==0 or abs(to_y - from_y)==1 )):
+    if (to_x == (from_x - 1) and (abs(to_y - from_y) == 0 or abs(to_y - from_y) == 1 )):
         return True
   if (firsttap == "bp" and first_char != "b" ):
     if (from_x == 1):
       if (to_x == 3 and to_y == from_y):
         return True
-    if (to_x == (from_x + 1) and (abs(to_y - from_y)==0 or abs(to_y - from_y)==1 )):
+    if (to_x == (from_x + 1) and (abs(to_y - from_y) == 0 or abs(to_y - from_y) == 1 )):
         return True
   else:
     return False
