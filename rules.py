917606afd6f3f5b03d6abd981bb8b4cdad93d040
Gurmeet-Singh745 <60310651+Gurmeet-Singh745@users.noreply.github.com>
2020-01-26 22:13:42 +0530

    Update rules.py

--- rules.py
+++ rules.py
@@ -20,8 +20,39 @@ def check(pieces, firsttap, from_x, from_y, to_x, to_y):
         return True
     if (to_x == (from_x + 1) and (abs(to_y - from_y) == 0 or abs(to_y - from_y) == 1 )):
         return True
-  
-  
+      
+   #bishop_rules
+
+
+  if(firsttap == "wb" and ( abs(to_x - from_x) == abs(to_y - from_y))):
+    for i, j in zip(range(from_x+1,8),range(from_y+1,8)):
+      curr_pos = pieces[i][j]
+      first_charcurr = curr_pos[0]
+      if (first_charcurr == "w"):
+        break
+      if( (to_x == i and to_y == j) and first_charfinal != "w" ):
+        return True
+    for i, j in zip(range(from_x-1,-1,-1),range(from_y-1,-1,-1)):
+      curr_pos = pieces[i][j]
+      first_charcurr = curr_pos[0]
+      if (first_charcurr == "w"):
+        break
+      if ((to_x == i and to_y == j) and first_charfinal != "w"):
+        return True
+    for i, j in zip( range(from_x + 1,8),range(from_y - 1,-1,-1) ):
+      curr_pos = pieces[i][j]
+      first_charcurr = curr_pos[0]
+      if (first_charcurr == "w"):
+        break
+      if ((to_x == i and to_y == j) and first_charfinal != "w"):
+        return True
+    for i, j in zip( range(from_x - 1, -1, -1), range(from_y + 1, 8) ):
+      curr_pos = pieces[i][j]
+      first_charcurr = curr_pos[0]
+      if (first_charcurr == "w"):
+        break
+      if ((to_x == i and to_y == j) and first_charfinal != "w"):
+        return True
   
   
   #Universal return False
