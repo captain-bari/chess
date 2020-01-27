302e1d908bce19ef49e7e822ab2b124f70bc08ce
Shourya-Kohli <60170174+Shourya-Kohli@users.noreply.github.com>
2020-01-27 22:32:15 +0530

    Update rules.py

--- rules.py
+++ rules.py
@@ -70,7 +70,43 @@ def check(pieces, firsttap, from_x, from_y, to_x, to_y):
       if ((to_x == i and to_y == j) and first_charfinal != "w"):
         return True
        
-       
+  #basic king rules
+    if firsttap == "bk" or firsttap == "wk":
+        x1 = from_x + 1
+        y1 = from_y + 1
+        if (x1 == to_x and y1 == to_y):
+            return True
+        x2 = from_x - 1
+        y2 = from_y - 1
+        if (x2 == to_x and y2 == to_y):
+            return True
+        x3 = from_x
+        y3 = from_y - 1
+        if (x3 == to_x and y3 == to_y):
+            return True
+        x4 = from_x
+        y4 = from_y + 1
+        if (x4 == to_x and y4 == to_y):
+            return True
+        x5 = from_x + 1
+        y5 = from_y
+        if (x5 == to_x and y5 == to_y):
+            return True
+        x6 = from_x - 1
+        y6 = from_y
+        if (x6 == to_x and y6 == to_y):
+            return True
+        x7 = from_x - 1
+        y7 = from_y + 1
+        if (x7 == to_x and y7 == to_y):
+            return True
+        x8 = from_x + 1
+        y8 = from_y - 1
+        if (x8 == to_x and y8 == to_y):
+            return True
+        else:
+            return False
+         
        
    #knight rules
   if firsttap == "bh" or firsttap == "wh":
