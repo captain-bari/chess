4590651233c5635893abf8ca4f52cc331ed60fa2
Shourya-Kohli <60170174+Shourya-Kohli@users.noreply.github.com>
2020-01-28 09:55:55 +0530

    Update rules.py

--- rules.py
+++ rules.py
@@ -110,7 +110,65 @@ def check(pieces, firsttap, from_x, from_y, to_x, to_y):
         else:
             return False
          
-       
+    #queen rules
+    if ((firsttap == "wq" or firsttap == "bq") and (abs(to_x - from_x) == abs(to_y - from_y))):
+        for i, j in zip(range(from_x + 1, 8), range(from_y + 1, 8)):
+            curr_pos = pieces[i][j]
+            first_charcurr = curr_pos[0]
+            if ((to_x == i and to_y == j)):
+                return True
+            if (first_charcurr == "w" or first_charcurr == "b"):
+                break
+
+        for i, j in zip(range(from_x - 1, to_x - 1, -1), range(from_y - 1, to_y - 1, -1)):
+            curr_pos = pieces[i][j]
+            first_charcurr = curr_pos[0]
+            if ((to_x == i and to_y == j)):
+                return True
+            if (first_charcurr == "w" or first_charcurr == "b"):
+                break
+
+        for i, j in zip(range(from_x + 1, 8), range(from_y - 1, -1, -1)):
+            curr_pos = pieces[i][j]
+            first_charcurr = curr_pos[0]
+            if ((to_x == i and to_y == j)):
+                return True
+            if (first_charcurr == "w" or first_charcurr == "b"):
+                break
+
+        for i, j in zip(range(from_x - 1, -1, -1), range(from_y + 1, 8)):
+            curr_pos = pieces[i][j]
+            first_charcurr = curr_pos[0]
+            if ((to_x == i and to_y == j)):
+                return True
+            if (first_charcurr == "w" or first_charcurr == "b"):
+                break
+    if firsttap == "wq" or firsttap == "bq":
+        if from_x == to_x:
+            if from_y > to_y:
+                for i in range(to_y + 1, from_y):
+                    if pieces[from_x][i] != "-":
+                        return False
+                return True
+
+            if from_y < to_y:
+                for i in range(from_y + 1, to_y):
+                    if pieces[from_x][i] != "-":
+                        return False
+                return True
+
+        if from_y == to_y:
+            if from_x > to_x:
+                for i in range(to_x + 1, from_x):
+                    if pieces[i][from_y] != "-":
+                        return False
+                return True
+            if from_x < to_x:
+                for i in range(from_x + 1, to_x):
+                    if pieces[i][from_y] != "-":
+                        return False
+                return True
+   
    #knight rules
   if firsttap == "bh" or firsttap == "wh":
     print("knight move")
