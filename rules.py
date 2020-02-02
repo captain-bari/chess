f2427c7f678fd296e819dd074fa47e71883de39b
Shourya-Kohli <60170174+Shourya-Kohli@users.noreply.github.com>
2020-02-02 18:29:23 +0530

    Update rules.py

--- rules.py
+++ rules.py
@@ -14,29 +14,29 @@ def check(pieces, firsttap, from_x, from_y, to_x, to_y):
 
 # pawn_rules
 
-  
-  
-  
-  
   final_pos = pieces[to_x][to_y]
   first_char = final_pos[0]
-  if(firsttap == "wp" and first_char != "w"):
-    if(to_y != from_y and pieces[to_x][to_y] == "-"):
-        return False
-    if(from_x == 6):
-      if(to_x==4 and to_y == from_y):
-        return True
-    if (to_x == (from_x - 1) and (abs(to_y - from_y) == 0 or abs(to_y - from_y) == 1 )):
-        return True
-  if (firsttap == "bp" and first_char != "b" ):
-    if (to_y != from_y and pieces[to_x][to_y] == "-"):
-      return False
-    if (from_x == 1):
-      if (to_x == 3 and to_y == from_y):
-        return True
-    if (to_x == (from_x + 1) and (abs(to_y - from_y) == 0 or abs(to_y - from_y) == 1 )):
-        return True
-      
+  if (firsttap == "wp" and first_char != "w"):
+      if (to_y != from_y and pieces[to_x][to_y] == "-"):
+          return False
+      if (from_x == 6):
+          if (to_x == 4 and to_y == from_y):
+              return True
+      if (to_x == (from_x - 1) and ( abs(to_y - from_y) == 0)and pieces[to_x][to_y]=="-"):
+          return True
+      if (to_x == (from_x - 1) and (abs(to_y - from_y) == 1)):
+          return True
+  if (firsttap == "bp" and first_char != "b"):
+      if (to_y != from_y and pieces[to_x][to_y] == "-"):
+          return False
+      if (from_x == 1):
+          if (to_x == 3 and to_y == from_y):
+              return True
+      if (to_x == (from_x + 1) and ( abs(to_y - from_y) == 0)and pieces[to_x][to_y]=="-"):
+          return True
+      if (to_x == (from_x + 1) and ( abs(to_y - from_y) == 1)):
+          return True
+
    #bishop_rules
 
 
